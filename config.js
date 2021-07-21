a7e6e5b284b70e5a8194d65eff4103b10101d513
TungThaiNguyen <87720519+TungThaiNguyen@users.noreply.github.com>
2021-07-21 11:12:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Chị Giang à!',
-    introDesc: `Tớ không thích xem “Mười vạn câu hỏi vì sao” xíu nào cả. Trong đó chẳng giải thích được vì sao em thích chị <3333`,
+    introDesc: `Em không thích xem “Mười vạn câu hỏi vì sao” xíu nào cả. Trong đó chẳng giải thích được vì sao em thích chị <3333`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: ' Ask me why I’m so happy and I’ll give you a mirror. 😙',
+    desc: 'Once you start loving someone, it’s hard to stop ',
     btnYes: 'Em biết chị thương em màaa <33',
     btnNo: 'Không, Em gái eiii :3',
     question:'Chị không phải kiểu người em thích, nhưng chị là người em thích <3',
     btnReply: 'Gửi cho em vài dòng tin nhắn điiii <3',
-    reply: 'Yêu thì yêu mà không yêu thì buộc yêuuuu <33333333',
+    reply: 'If you love, you will love, if you do not love, you will love <33333333',
     mess: 'Em biết là chị yêu em mà 🥰. Yêu chị nhiều nhiều 😘😘',
-    messDesc: 'Chị ạ, em nghĩ là nếu chị dạy em thì sắp tới em sẽ nghịch chị nhiều đấy😁😁😁😁.Hớt gòi, chị hãy quay lại nhắn với em  đi 🥰🥰🥰🥰',
+    messDesc: 'Chị ạ, em nghĩ là sắp tới em sẽ nghịch chị nhiều đấy nhưng chỉ khi nào chị rảnh thooooiii ( làm 13-14h/ngày thì rảnh khi nào nhỉ?)😁😁😁😁.Hớt gòi, chị hãy quay lại nhắn với em  đi 🥰🥰🥰🥰',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100010913476138' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
